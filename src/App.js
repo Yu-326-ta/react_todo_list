e7618ea678b530f9142c25b5cdd926d39da479cf
Yu-326-ta <yoshinagayuuta@outlook.jp>
2022-09-17 01:52:59 +0900

    入力値を受け取る実装

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import { InputForm } from './components/InputForm';
 import {Title} from "./components/Title";
+import { TodoList } from './components/TodoList';
 
 function App() {
+
+  const [taskList, setTaskList] = useState([]);
+
   return (
     <div className="body">
       <Title />
-      <InputForm />
+      <InputForm taskList={taskList} setTaskList={setTaskList} />
+      <TodoList />
     </div>
   );
 }
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const InputForm = () => {
+export const InputForm = ({taskList, setTaskList}) => {
+
+  const [inputText, setInputText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTaskList([
+      ...taskList,
+      {
+        text: inputText
+      }
+    ]);
+  }
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  }
+
   return (
     <div className="inputForm">
-        <form>
-            <input type="text" />
+        <form onSubmit={handleSubmit}>
+            <input type="text" onChange={handleChange}/>
             <button>
             <i className="fas fa-plus-square"></i>
             </button>
