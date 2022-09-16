7e7a06be9c6cfd10a987022a01e54ef3a7ca6b34
Yu-326-ta <yoshinagayuuta@outlook.jp>
2022-09-17 00:59:15 +0900

    フォームの実装

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const InputForm = () => {
+  return (
+    <div className="inputForm">
+        <form>
+            <input type="text" />
+            <button>
+            <i className="fas fa-plus-square"></i>
+            </button>
+        </form>
+    </div>
+ )
+}
